de8c254510db0c8a00d9b3ab7b1222544e5c1307
Nishkarsh <Nishkarshthakur78@gmail.com>
2025-09-05 12:40:22 +0530

    Added two sum solution in Arrays

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,12 +1,16 @@
+// Two Sum Problem
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++){
+            // second iteration should be start from i+1 to avoid same index conflict
             for(int j=i+1; j<nums.length; j++){
                 if(nums[i] + nums[j] == target){
                     return new int[]{i, j};
                 }
             }
         }
+        // Return empty array if target not exist
         return new int[]{};
     }
 }
