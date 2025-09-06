bf3adca8a75a5a0c6ca9f0cd9963f1a765b6cfa3
Nishkarsh <Nishkarshthakur78@gmail.com>
2025-09-06 09:14:06 +0530

    Added Reverse String solution in Strings

---
+++ Strings/ReverseString.java
@@ -0,0 +1,18 @@
+// Reverse String
+// Link = https://leetcode.com/problems/reverse-string/description/
+
+// Approach : Two pointer
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right ){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
